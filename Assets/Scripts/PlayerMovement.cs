4426bb01d23dc922e315b90ab0ea4b25561283bb
Craig Franceschetti <cfranceschetti33@rrc.ca>
2023-10-25 20:52:53 -0500

    Made a very basic player controller, and baseplayer class to start things off.

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private Vector2 horizonalInput = Vector2.zero;
+    //public to debug
+    public float playerSpeed = 500f;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        //Just left and right movement for now?? not sure.
+        horizonalInput = new Vector2(Input.GetAxis("Horizontal"), 0f);
+    }
+
+    private void FixedUpdate()
+    {
+        //Processing the horizontal movement.
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D))
+        {
+            Debug.Log(horizonalInput);
+            rb.AddForce(horizonalInput * playerSpeed * Time.deltaTime);
+        }
+    }
+}
