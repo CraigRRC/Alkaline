45f7c5df62069ed0186df2236e8b614b75c7c1dd
rparedes2 <144839241+RussRRC@users.noreply.github.com>
2023-11-06 11:45:07 -0600

    Adjusted player groundCheck position on prefab

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -157,7 +157,7 @@ public class PlayerMovement : MonoBehaviour
 
     private bool IsGrounded()
     {
-        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+        return Physics2D.OverlapCircle(groundCheck.position, 0.1f, groundLayer);
     }
 }
 
