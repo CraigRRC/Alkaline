6eccc47a0bf89bdd9093cae5a444abea381cf055
Zachery <zgilbert@rrc.ca>
2023-11-14 17:06:25 -0600

    some moving around of assets and changing of the swich script

--- Assets/Scripts/SwitchTheMagnet.cs
+++ Assets/Scripts/SwitchTheMagnet.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class SwitchTheMagnet : MonoBehaviour
 {
-    [Header("Switch them on to off or vise versa?")]
-    public bool OnToOff;
+    [Header("Switch them on to off or vise versa")]
+    public bool OnOff;
+    private bool isOn = true;
 
     //for only a certain amount of switches
     public int maxSwitches;
@@ -13,21 +14,44 @@ public class SwitchTheMagnet : MonoBehaviour
 
     public Magnet[] magnets;
 
+    private Animator switchAnimator;
+
+
+    private void Awake()
+    {
+        switchAnimator = GetComponent<Animator>();
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+       switchAnimator.enabled = true;
+
         if (magnets.Length <= 0) Debug.LogWarning("Nothing in Magnet array!!");
 
         if(switchedCount < maxSwitches)
         {
             for (int i = 0; i < magnets.Length; i++)
             {
-                if (OnToOff) magnets[i].TurnMagnetOff();
+                if (OnOff) 
+                {
+                    if (isOn)
+                    {
+                        magnets[i].TurnMagnetOff();
+                        isOn = false;
+                        Debug.Log("Magnet/s turned off");
+                    }
+                    else
+                    {
+                        magnets[i].TurnMagnetOn();
+                        isOn = true;
+                        Debug.Log("Magnet/s turned on");
+                    }
+                } 
                 else magnets[i].FlipPolarity();
+                Debug.Log("Magnet/s flipped polarity");
             }
-
-            if (OnToOff) Debug.Log("Magnet/s turned off");
-            else Debug.Log("Magnet/s flipped");
+            /*if (OnOff) Debug.Log("Magnet/s turned off");
+            else Debug.Log("Magnet/s flipped");*/
             switchedCount++;
         }
         else Debug.Log("Hit max amount on switch");
