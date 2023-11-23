71bed5fbf4b421ce90a59f827507ad61c570d405
Craig Franceschetti <cfranceschetti33@rrc.ca>
2023-11-23 14:50:19 -0600

    Made 3 scripts that need to be completed before the next milestone. Made them all now so that I dont forget.

---
+++ Assets/Scripts/InDevelopment/UI.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+//Class that will be used to handle the UI for the player
+//Changes to the UI will be made dynamically and persist.
+//Needs to handle various things like the battery meter, and the logs that have been acquired.
+public class UI : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
