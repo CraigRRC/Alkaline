4a982fa471744e29fabcd316b66f4129dc972c37
Craig Franceschetti <cfranceschetti33@rrc.ca>
2023-11-27 19:59:48 -0600

    Working Log persistance... Somewhat. Needs more testing though.

---
+++ Assets/Scripts/InDevelopment/LogsData.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LogsData : MonoBehaviour
+{
+    public Text[] logs;
+    public Image[] logImages;
+
+    private void Awake()
+    {
+        logs = GetComponentsInChildren<Text>();
+        logImages = GetComponentsInChildren<Image>();
+        
+    }
+}
