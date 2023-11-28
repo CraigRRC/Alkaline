299775ff7c28ea44dff00afdf836c86ed074c918
Zachery <zgilbert@rrc.ca>
2023-11-28 12:02:37 -0600

    first commit, started camera shake script

--- Assets/Scripts/InDevelopment/CameraShake.cs
+++ Assets/Scripts/InDevelopment/CameraShake.cs
@@ -1,11 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 //Shake camera when a box collides with the ground.
 //Can be turned on or off per level/options menu.
 public class CameraShake : MonoBehaviour
 {
+    public Camera cam;
+    private bool isShaking;
+
+     /*if (collision.relativeVelocity.magnitude > lethalImpactForce)
+        {
+            PlayerDead();
+        }*/
+
+    
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,5 +28,11 @@ public class CameraShake : MonoBehaviour
     void Update()
     {
         
+    }
+
+    void ShakeCam(float ShakeDuration)
+    {
+        isShaking = true;
+
     }
 }
