348cf68f31d04d1a0712de69181eee21fbb0a093
Zachery <zgilbert@rrc.ca>
2023-12-05 16:35:53 -0600

    made and broke code CURRENTLY BREAKS GAME or crashes it

--- Assets/Scripts/InDevelopment/CameraShake.cs
+++ Assets/Scripts/InDevelopment/CameraShake.cs
@@ -9,30 +9,55 @@ using UnityEngine.Rendering;
 public class CameraShake : MonoBehaviour
 {
     public Camera cam;
+    private Vector2 origonalCamPos;
+    public float impactForce = 2f;
     private bool isShaking;
+    public bool canShake = true;
+    public float shakeDuration = 1.0f;
+    private float elapsedTime  = 0.0f;
 
      /*if (collision.relativeVelocity.magnitude > lethalImpactForce)
         {
             PlayerDead();
         }*/
 
-    
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        if(cam == null)
+        {
+            Debug.Log("No cam to shake");
+            canShake = false;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 
-    void ShakeCam(float ShakeDuration)
+    private void OnCollisionEnter2D(Collision2D collision)
     {
+        if(canShake && collision.relativeVelocity.magnitude > impactForce && isShaking == false)
+        {
+            ShakeCam(shakeDuration);
+        }
+    }
+
+    void ShakeCam(float shakeDuration)
+    {
+        origonalCamPos = cam.transform.position;
         isShaking = true;
 
+        while (elapsedTime < shakeDuration)
+        {
+            float x = Random.Range(-1f, 1f);
+            float y = Random.Range(-1f, 1f);                                //CURRENTLY EXTREEEEEEEMLY BROKEN DO NOT USE (yet)
+            transform.position = new Vector2(x, y/*, origonalCamPos.z*/);
+        }
+
+        transform.position = origonalCamPos;
     }
 }
